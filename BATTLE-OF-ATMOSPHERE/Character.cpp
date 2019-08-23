00844941541ded7b2d60e4035ee0417a3524352f
上久保豪太 <kamikubo1123@gmail.com>
2019-08-23 16:44:46 +0900

    :construction: 自機用クラス作成

---
+++ BATTLE-OF-ATMOSPHERE/Character.cpp
@@ -0,0 +1,55 @@
+﻿#include"../BATTLE-OF-ATMOSPHERE/Character.h"
+#include <time.h>
+
+VOID SPACEMAN1P::SpaceManJump()
+{
+
+	SPACEMAN1P::m_pos_y -= SPACEMAN1P::jump_budget;
+	SPACEMAN1P::jump_budget -= SPACEMAN1P::m_gravity;
+}
+
+VOID SPACEMAN1P::SpaceManMove()
+{
+
+	if (directx.KeyState[DIK_D] == directx.ON)
+	{
+		SPACEMAN1P::m_pos_x += SPACEMAN1P::m_spaceman_speed;
+
+	}
+
+	if (directx.KeyState[DIK_A] == directx.ON)
+	{
+		SPACEMAN1P::m_pos_x -= SPACEMAN1P::m_spaceman_speed;
+
+	}
+
+	if (directx.KeyState[DIK_W] == directx.PRESS)
+	{
+		SPACEMAN1P::m_coordinate_storing = SPACEMAN1P::m_pos_y;
+		SPACEMAN1P::m_gravity = 0.25;
+		SPACEMAN1P::m_is_jump = true;
+	}
+
+	if (directx.KeyState[DIK_S] == directx.ON)
+	{
+		SPACEMAN1P::m_pos_y += SPACEMAN1P::m_spaceman_speed;
+	}
+
+	if (SPACEMAN1P::m_is_jump == true)
+	{
+		SpaceManJump();
+	}
+	else
+	{
+		SPACEMAN1P::m_pos_y += SPACEMAN1P::m_gravity;
+		SPACEMAN1P::m_gravity += 0.25;
+	}
+
+	if (SPACEMAN1P::m_pos_y >= 825)
+	{
+		SPACEMAN1P::jump_budget = 15.0F;
+		SPACEMAN1P::m_is_jump = false;
+	}
+
+
+}
---
+++ BATTLE-OF-ATMOSPHERE/Character.h
@@ -0,0 +1,45 @@
+﻿#ifndef CHARACTER_H_
+#define CHARACTER_H_
+
+#include "../BATTLE-OF-ATMOSPHERE/DirectX.h"
+#include "../BATTLE-OF-ATMOSPHERE/Main.h"
+#include "../BATTLE-OF-ATMOSPHERE/MapChip.h"
+
+/**
+* @brief 自機のステータスのベース
+*/
+class SPACEMANBASE
+{
+public:
+	INT m_spaceman_speed = 5.0F;		//! 自機の移動速度
+	FLOAT m_gravity = 0.25F;				//! 自機にかかる重力(月と同じ)
+	FLOAT m_pos_x = 0.0F;				//! 自機のX座標
+	FLOAT m_pos_y = 0.0F;				//! 自機のY座標
+	FLOAT jump_budget = 15.0F;			//! ジャンプの速度
+	FLOAT m_spaceman_width = 128.0F;	//! 自機の幅
+	FLOAT m_spaceman_height = 256.0F;	//! 自機の高さ
+
+	/**
+	* @fn virtual VOID SpaceManMove()
+	* @brief 自機の移動用仮想関数
+	*/
+	virtual VOID SpaceManMove() = 0;
+
+private:
+
+protected:
+	FLOAT m_max_jump_power = 384.0;		//! 自機の最大ジャンプ力
+	FLOAT m_coordinate_storing = 0;		//! 現在の座標記憶用変数
+	FLOAT m_max_jump_budget = 0;		//! 最大ジャンプ量
+	BOOL m_is_jump = false;
+
+};
+
+class SPACEMAN1P :public SPACEMANBASE
+{
+public:
+	VOID SpaceManJump();
+	VOID SpaceManMove();
+};
+
+#endif // !CHARACTER_H_
