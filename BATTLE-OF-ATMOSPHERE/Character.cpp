ced955a1ef636a7cfb637eb9a8052f5693adf117
上久保豪太 <kamikubo1123@gmail.com>
2019-08-29 13:03:40 +0900

    :art: 見やすいように関数を追加

--- BATTLE-OF-ATMOSPHERE/Character.cpp
+++ BATTLE-OF-ATMOSPHERE/Character.cpp
@@ -1,98 +1,41 @@
 ﻿#include"../BATTLE-OF-ATMOSPHERE/Character.h"
-#include <time.h>
 
-VOID SPACEMANBASE::SpaceManRelease()
+VOID SPACEMAN1P::SpaceManRelease()
 {
-	//着地時にジャンプ関係の変数を初期化
-	if (SPACEMANBASE::m_pos_y >= 890)
+	// 仮の地面の座標(890)
+	if (SPACEMAN1P::m_pos_y >= 890)
 	{
-		SPACEMANBASE::m_jump_power = SPACEMANBASE::m_max_jump_power;
-		SPACEMANBASE::m_jump = NO_JAMP;
-		SPACEMANBASE::m_is_call = FALSE;
-		SPACEMANBASE::m_is_dash = TRUE;
+		SPACEMAN1P::m_jump_power = SPACEMAN1P::m_max_jump_power;
+		SPACEMAN1P::m_jump = NO_JAMP;
+		SPACEMAN1P::m_is_call = FALSE;
 	}
 	else
 	{
-		SPACEMANBASE::m_plus_spaceman_speed = 0.0F;
-		SPACEMANBASE::m_is_dash = FALSE;
-	}
-}
-
-VOID SPACEMAN1P::SpaceManJump()
-{
-	SPACEMAN1P::m_pos_y -= SPACEMAN1P::m_jump_power;
-	SPACEMAN1P::m_jump_power -= SPACEMAN1P::m_gravity;
-}
-
-VOID SPACEMAN1P::SpaceManDash()
-{
-	if (SPACEMAN1P::m_is_dash == TRUE) {
-
-		if ((directx.KeyState[DIK_D] == directx.ON &&
-			directx.KeyState[DIK_G] == directx.ON) ||
-			(directx.KeyState[DIK_A] == directx.ON &&
-				directx.KeyState[DIK_G] == directx.ON))
-		{
-
-			SPACEMAN1P::m_plus_spaceman_speed += 0.05F;
-			SPACEMAN1P::m_plus_jump_power += 0.05F;
-
-			if (SPACEMAN1P::m_plus_spaceman_speed >= 5.0F)
-			{
-				SPACEMAN1P::m_plus_spaceman_speed = 5.0F;
-				SPACEMAN1P::m_plus_jump_power = 5.0F;
-			}
-		}
-		else if (directx.KeyState[DIK_G] == directx.OFF)
-		{
-			SPACEMAN1P::m_plus_spaceman_speed = 0.0F;
-			SPACEMAN1P::m_plus_jump_power = 0.0F;
-		}
-	}
-}
-
-VOID SPACEMAN1P::SpaceManMove()
-{
-	SPACEMAN1P::SpaceManRelease();
-
-	if (directx.KeyState[DIK_D] == directx.ON)
-	{
-		SPACEMAN1P::m_pos_x += (SPACEMAN1P::m_spaceman_speed + SPACEMAN1P::m_plus_spaceman_speed);
-	}
-
-	if (directx.KeyState[DIK_A] == directx.ON)
-	{
-		SPACEMAN1P::m_pos_x -= (SPACEMAN1P::m_spaceman_speed + SPACEMAN1P::m_plus_spaceman_speed);
-	}
-
-	SpaceManDash();
-
-	if (directx.KeyState[DIK_W] == directx.PRESS)
-	{
-		SPACEMAN1P::m_gravity = SPACEMAN1P::m_max_gravity;
-		SPACEMAN1P::m_jump_power = (SPACEMAN1P::m_jump_power + SPACEMAN1P::m_plus_jump_power);
-
-		switch (SPACEMAN1P::m_jump)
-		{
-		case NO_JAMP:
-			SPACEMAN1P::m_jump = ONE_JAMP;
-			break;
-		case ONE_JAMP:
-			SPACEMAN1P::m_jump = TWO_JAMP;
-			break;
-		}
-	}
-
-	if (directx.KeyState[DIK_S] == directx.ON)
-	{
-		SPACEMAN1P::m_pos_y += SPACEMAN1P::m_spaceman_speed;
+		SPACEMAN1P::m_plus_spaceman_speed = 0.0F;
 		SPACEMAN1P::m_is_dash = FALSE;
 	}
+}
 
-	switch (SPACEMAN1P::m_jump)
+VOID SPACEMAN1P::SpaceManJumpSwitchChange(INT* jump)
+{
+	switch (*jump)
+	{
+	case NO_JAMP:
+		*jump = ONE_JAMP;
+		break;
+	case ONE_JAMP:
+		*jump = TWO_JAMP;
+		break;
+	}
+}
+
+VOID SPACEMAN1P::SpaceManSwitchJump(INT jump)
+{
+	switch (jump)
 	{
 	case NO_JAMP:
 
+		// ジャンプしていないとき（落下など）自身に重力をかけている
 		SPACEMAN1P::m_pos_y += SPACEMAN1P::m_gravity;
 		SPACEMAN1P::m_gravity += SPACEMAN1P::m_max_gravity;
 		break;
@@ -116,5 +59,74 @@ VOID SPACEMAN1P::SpaceManMove()
 
 		break;
 	}
+}
+
+VOID SPACEMAN1P::SpaceManJump()
+{
+	SPACEMAN1P::m_pos_y -= SPACEMAN1P::m_jump_power;
+	SPACEMAN1P::m_jump_power -= SPACEMAN1P::m_gravity;
+}
+
+VOID SPACEMAN1P::SpaceManDash()
+{
+	if (SPACEMAN1P::m_is_dash == TRUE) {
+
+		if ((directx.KeyState[DIK_D] == directx.ON &&
+			directx.KeyState[DIK_G] == directx.ON) ||
+			(directx.KeyState[DIK_A] == directx.ON &&
+				directx.KeyState[DIK_G] == directx.ON))
+		{
+
+			SPACEMAN1P::m_plus_spaceman_speed += SPACEMAN1P::m_plus_dash_power;
+			SPACEMAN1P::m_plus_jump_power += SPACEMAN1P::m_plus_dash_power;
+
+			if (SPACEMAN1P::m_plus_spaceman_speed >= SPACEMAN1P::m_max_plus_spaceman_power)
+			{
+				SPACEMAN1P::m_plus_spaceman_speed = SPACEMAN1P::m_max_plus_spaceman_power;
+				SPACEMAN1P::m_plus_jump_power = SPACEMAN1P::m_max_plus_spaceman_power;
+			}
+		}
+		else if (directx.KeyState[DIK_G] == directx.OFF)
+		{
+			SPACEMAN1P::m_plus_spaceman_speed = 0.0F;
+			SPACEMAN1P::m_plus_jump_power = 0.0F;
+		}
+	}
+}
+
+VOID SPACEMAN1P::SpaceManMove()
+{
+	SPACEMAN1P::SpaceManRelease();
+
+	if (directx.KeyState[DIK_D] == directx.ON)
+	{
+		SPACEMAN1P::m_pos_x += (SPACEMAN1P::m_spaceman_speed + SPACEMAN1P::m_plus_spaceman_speed);
+	}
+	else if (directx.KeyState[DIK_A] == directx.ON)
+	{
+		SPACEMAN1P::m_pos_x -= (SPACEMAN1P::m_spaceman_speed + SPACEMAN1P::m_plus_spaceman_speed);
+	}
+
+	SPACEMAN1P::SpaceManDash();
+
+	if (directx.KeyState[DIK_W] == directx.PRESS)
+	{
+		SPACEMAN1P::m_gravity = SPACEMAN1P::m_max_gravity;
+		SPACEMAN1P::m_jump_power = (SPACEMAN1P::m_jump_power + SPACEMAN1P::m_plus_jump_power);
+
+		SPACEMAN1P::SpaceManJumpSwitchChange(&m_jump);
+	}
+
+	if (directx.KeyState[DIK_S] == directx.ON)
+	{
+		SPACEMAN1P::m_pos_y += SPACEMAN1P::m_spaceman_speed;
+		SPACEMAN1P::m_is_dash = FALSE;
+	}
+	else if (directx.KeyState[DIK_S] == directx.OFF)
+	{
+		SPACEMAN1P::m_is_dash = TRUE;
+	}
+
+	SPACEMAN1P::SpaceManSwitchJump(SPACEMAN1P::m_jump);
 
 }
--- BATTLE-OF-ATMOSPHERE/Character.h
+++ BATTLE-OF-ATMOSPHERE/Character.h
@@ -11,34 +11,38 @@
 class SPACEMANBASE
 {
 public:
-	FLOAT m_spaceman_speed = 5.0F;			//! 自機の移動速度
-	FLOAT m_plus_spaceman_speed = 0.0F;
 
-	CONST FLOAT m_max_gravity = 0.2F;		//! 重力の定数
-	FLOAT m_gravity = 0.0F;					//! 自機にかかる重力
+	// 歩行関係変数
+	FLOAT m_spaceman_speed = 5.0F;					//! 自機の移動速度
 
-	FLOAT m_pos_x = 0.0F;					//! 自機のX座標
-	FLOAT m_pos_y = 0.0F;					//! 自機のY座標
-	FLOAT m_spaceman_width = 96.0F;			//! 自機の幅
-	FLOAT m_spaceman_height = 192.0F;		//! 自機の高さ
+	// 重力関係変数
+	CONST FLOAT m_max_gravity = 0.2F;				//! 重力の定数
+	FLOAT m_gravity = 0.0F;							//! 自機にかかる重力
 
-	CONST FLOAT m_max_jump_power = 10.0F;	//! ジャンプ速度の定数
-	FLOAT m_jump_power = 0.0F;				//! ジャンプの速度
-	FLOAT m_plus_jump_power = 0.0F;
+	// 自機の座標、サイズ関係変数
+	FLOAT m_pos_x = 0.0F;							//! 自機のX座標
+	FLOAT m_pos_y = 0.0F;							//! 自機のY座標
+	FLOAT m_spaceman_width = 96.0F;					//! 自機の幅
+	FLOAT m_spaceman_height = 192.0F;				//! 自機の高さ
+
+	// ジャンプ関係変数
+	CONST FLOAT m_max_jump_power = 10.0F;			//! ジャンプ速度の定数
+	FLOAT m_jump_power = 0.0F;						//! ジャンプの速度
+	FLOAT m_plus_jump_power = 0.0F;					//! ダッシュ時、上昇したジャンプ量
+
+	// ダッシュ関係変数
+	FLOAT m_plus_spaceman_speed = 0.0F;				//! ダッシュ時の現在の速度加速量
+	CONST FLOAT m_max_plus_spaceman_power = 5.0F;	//! ダッシュ時の最大加速量
+	CONST FLOAT m_plus_dash_power = 0.05F;			//! ダッシュの加速度
+
 
 
 	enum JAMP
 	{
-		NO_JAMP,
-		ONE_JAMP,
-		TWO_JAMP
+		NO_JAMP,	//! ジャンプしていない
+		ONE_JAMP,	//! 1回ジャンプした
+		TWO_JAMP	//! 二段ジャンプした
 	};
-
-	/**
-	* @fn VOID SpaceManRelease()
-	* @brief 自機のステータス解放用関数
-	*/
-	VOID SpaceManRelease();
 
 	/**
 	* @fn virtual VOID SpaceManDash()
@@ -52,11 +56,8 @@ public:
 	*/
 	virtual VOID SpaceManMove() = 0;
 
-private:
-
 protected:
 	INT m_jump = NO_JAMP;				//! ジャンプ状態記憶用変数
-	FLOAT m_coordinate_storing = 0;		//! 現在の座標記憶用変数
 	BOOL m_is_dash = FALSE;				//! 現在ジャンプが可能かどうかを判断する用フラグ
 	BOOL m_is_call = FALSE;				//! 呼ばれたかどうか判断する用フラグ
 };
@@ -67,6 +68,27 @@ public:
 	VOID SpaceManJump();
 	VOID SpaceManDash();
 	VOID SpaceManMove();
+
+	/**
+	* @fn VOID SpaceManRelease()
+	* @brief 自機のステータス解放用関数
+	*/
+	VOID SpaceManRelease();
+
+	/**
+	* @fn VOID SpaceManJumpSwitchChange(INT* jump)
+	* @param (INT jump) 現在のジャンプ状態
+	* @brief ジャンプボタンが押されたときジャンプ状態を切り替える
+	*/
+	VOID SpaceManJumpSwitchChange(INT* jump);
+
+	/**
+	* @fn VOID SpaceManSwitchJump(INT jump)
+	* @param (INT jump) 現在のジャンプ状態
+	* @brief 現在のジャンプ状態に応じて処理を行う関数
+	*/
+	VOID SpaceManSwitchJump(INT jump);
 };
 
 #endif // !CHARACTER_H_
+
